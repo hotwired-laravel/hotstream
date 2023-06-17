294be682018a584d3dd301048cb7af3c9c5b4989
Tony Messias <tonysm@hey.com>
2023-06-17 20:40:54 -0300

    Adds the InviteTeamMemberTest

---
+++ tests/InviteTeamMemberTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Actions\Hotstream\InviteTeamMember;
+use App\Models\Team;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\ValidationException;
+use Hotwired\Hotstream\Hotstream;
+use Hotwired\Hotstream\Tests\Fixtures\TeamPolicy;
+use Hotwired\Hotstream\Tests\Fixtures\User;
+
+beforeEach(function () {
+    Gate::policy(Team::class, TeamPolicy::class);
+    Hotstream::useUserModel(User::class);
+    migrate();
+});
+
+test('team members can be invited', function () {
+    Mail::fake();
+
+    Hotstream::role('admin', 'Admin', ['foo']);
+
+    $team = createTeam();
+
+    $otherUser = User::forceCreate([
+        'name' => 'Adam Wathan',
+        'email' => 'adam@laravel.com',
+        'password' => 'secret',
+    ]);
+
+    $action = new InviteTeamMember;
+
+    $action->invite($team->owner, $team, 'adam@laravel.com', 'admin');
+
+    $team = $team->fresh();
+
+    $this->assertCount(0, $team->users);
+    $this->assertCount(1, $team->teamInvitations);
+    $this->assertEquals('adam@laravel.com', $team->teamInvitations->first()->email);
+    $this->assertEquals($team->id, $team->teamInvitations->first()->team->id);
+});
+
+test('user cant already be on team', function () {
+    Mail::fake();
+
+    $this->expectException(ValidationException::class);
+
+    $team = createTeam();
+
+    $otherUser = User::forceCreate([
+        'name' => 'Adam Wathan',
+        'email' => 'adam@laravel.com',
+        'password' => 'secret',
+    ]);
+
+    $action = new InviteTeamMember;
+
+    $action->invite($team->owner, $team, 'adam@laravel.com', 'admin');
+    $this->assertTrue(true);
+    $action->invite($team->owner, $team->fresh(), 'adam@laravel.com', 'admin');
+});
