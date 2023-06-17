d3f68199b62ae912afeb082bc342106a22285de6
Tony Messias <tonysm@hey.com>
2023-06-17 18:08:33 -0300

    Upgrade the phpunit.xml.dist and adds the CreateTeamTest

---
+++ tests/CreateTeamTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Actions\Hotstream\CreateTeam;
+use App\Models\Team;
+use Hotwired\Hotstream\Hotstream;
+use Hotwired\Hotstream\Tests\Fixtures\TeamPolicy;
+use Hotwired\Hotstream\Tests\Fixtures\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\ValidationException;
+
+beforeEach(function () {
+    Gate::policy(Team::class, TeamPolicy::class);
+    Hotstream::useUserModel(User::class);
+    migrate();
+});
+
+test('can create team', function () {
+    $action = new CreateTeam;
+
+    $user = User::forceCreate([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $team = $action->create($user, ['name' => 'Test Team']);
+
+    $this->assertInstanceOf(Team::class, $team);
+});
+
+test('name is required', function () {
+    $this->expectException(ValidationException::class);
+
+    $action = new CreateTeam;
+
+    $user = User::forceCreate([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $action->create($user, ['name' => '']);
+});
