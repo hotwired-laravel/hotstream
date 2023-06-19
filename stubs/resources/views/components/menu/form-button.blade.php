03078d41d5f06f016076b4e3704eca292c8f17c8
Tony Messias <tonysm@hey.com>
2023-06-19 16:02:24 -0300

    Fix the menu items components

--- stubs/resources/views/components/menu/form-button.blade.php
+++ stubs/resources/views/components/menu/form-button.blade.php
@@ -6,7 +6,7 @@
         @method($method)
     @endif
 
-    <button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full block text-gray-700 dark:text-white px-3 py-2 native:p-4 text-sm native:text-base flex items-center justify-start space-x-4 transition hover:bg-gray-50 hover:dark:bg-gray-800']) }}>
+    <button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full block text-gray-800 dark:text-white px-3 py-2 native:p-4 text-sm native:text-base flex items-center justify-start space-x-4 transition hover:bg-gray-50 hover:dark:bg-gray-800']) }}>
         @isset ($icon)
         <x-icon :icon="$icon" size="sm" />
         @endisset
--- stubs/resources/views/components/menu/link.blade.php
+++ stubs/resources/views/components/menu/link.blade.php
@@ -1,7 +1,7 @@
 @props(['icon' => null])
 
 <a {{ $attributes->merge([
-    'class' => 'block dark:text-white px-4 py-3 native:p-4 text-base flex items-center justify-between space-x-4 transition hover:bg-gray-50 hover:dark:bg-gray-800',
+    'class' => 'block text-gray-800 dark:text-white px-3 py-2 native:p-4 text-sm native:text-base flex items-center justify-between space-x-4 transition hover:bg-gray-50 hover:dark:bg-gray-800',
 ]) }}>
     <span class="flex items-center space-x-4">
         @isset ($icon)
